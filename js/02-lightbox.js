3f31883b32e6c7bd2fdcfed2267b6fcdf9fbfc64
bjpangulayan <bjspangulayan23@gmail.com>
2024-04-30 15:10:52 +0800

    Updated 01-gallery.js and 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector("ul.gallery");
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map((galleryItem) => {
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
+            </a></li>`
+         })
+        .join("");
+}
+
+const photosMarkup = createGalleryItem(galleryItems); 
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+const galleryHandler = new SimpleLightbox(".gallery a", {captionsData: "alt", captionDelay: 250});
+galleryHandler.on("show.simplelightbox");
